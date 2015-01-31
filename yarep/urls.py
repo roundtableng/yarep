d9b37ce4ecf7f26e346f3d675a40888a15a76bde
bayo <bayokrapht@gmail.com>
2015-01-31 14:29:06 +0100

    done home page

--- yarep/urls.py
+++ yarep/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'yarep.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', TemplateView.as_view(
+        template_name='core/home.html'), name="home"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
