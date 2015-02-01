3254c93f50b2aef114c40c92d516ddd663123559
bayo <bayokrapht@gmail.com>
2015-02-01 16:50:23 +0100

    moved files from static to otherstatic and fixed settings

--- yarep/settings.py
+++ yarep/settings.py
@@ -80,12 +80,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'otherstatic'),
 )
 
 # List of finder classes that know how to find static files in
